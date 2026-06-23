a1e12280eea4390af989c04067287409c3edd49e
anselhc <aharriscrowne@olin.edu>
2026-06-23 12:43:08 -0400

    Fixing abad merge

--- Firmware/me_gizmo/me_gizmo.cpp
+++ Firmware/me_gizmo/me_gizmo.cpp
@@ -3,8 +3,8 @@
 
 SPISettings ADC_SPISettings(2000000, MSBFIRST, SPI_MODE0);
 
-uint16_t dacA_val = 0x800, dacB_val = 0x800;
-uint8_t dacA_ena = 0, dacB_ena = 0;
+uint16_t dacA_val = 0x800, dacB_val = 0x800, dacC_val = 0x800, dacD_val = 0x800;
+uint8_t dacA_ena = 0, dacB_ena = 0, dacC_ena = 0, dacD_ena = 0;
 
 uint16_t adc_scan_lists[4] = { 0, 0, 0, 0 };
 
@@ -295,8 +295,11 @@ uint16_t adc_scan_once(uint8_t address, uint32_t *buffer) {
 
 // Digital-to-analog converter (DAC) functions
 void init_dac_pre_init_adc(void) {
-    pinMode(DAC_CSN, OUTPUT);
-    digitalWrite(DAC_CSN, HIGH);
+    pinMode(DAC1_CSN, OUTPUT);
+    digitalWrite(DAC1_CSN, HIGH);
+
+    pinMode(DAC2_CSN, OUTPUT);
+    digitalWrite(DAC2_CSN, HIGH);
 
     pinMode(DAC_LDAC, OUTPUT);
     digitalWrite(DAC_LDAC, HIGH);
@@ -305,18 +308,20 @@ void init_dac_pre_init_adc(void) {
 void init_dac_post_init_adc(void) {
     dac_update_dacA();
     dac_update_dacB();
+    dac_update_dacC();
+    dac_update_dacD();
     dac_load();
 }
 
 void dac_update_dacA(void) {
     SPI.beginTransaction(ADC_SPISettings);
 
-    digitalWrite(DAC_CSN, LOW);
+    digitalWrite(DAC1_CSN, LOW);
 
     SPI.transfer((dacA_ena ? 0x70 : 0x60) | (dacA_val >> 8));
     SPI.transfer(dacA_val & 0xFF);
 
-    digitalWrite(DAC_CSN, HIGH);
+    digitalWrite(DAC1_CSN, HIGH);
 
     SPI.endTransaction();
 }
@@ -324,12 +329,38 @@ void dac_update_dacA(void) {
 void dac_update_dacB(void) {
     SPI.beginTransaction(ADC_SPISettings);
 
-    digitalWrite(DAC_CSN, LOW);
+    digitalWrite(DAC1_CSN, LOW);
 
     SPI.transfer((dacB_ena ? 0xF0 : 0xE0) | (dacB_val >> 8));
     SPI.transfer(dacB_val & 0xFF);
 
-    digitalWrite(DAC_CSN, HIGH);
+    digitalWrite(DAC1_CSN, HIGH);
+
+    SPI.endTransaction();
+}
+
+void dac_update_dacC(void) {
+    SPI.beginTransaction(ADC_SPISettings);
+
+    digitalWrite(DAC2_CSN, LOW);
+
+    SPI.transfer((dacC_ena ? 0x70 : 0x60) | (dacC_val >> 8));
+    SPI.transfer(dacC_val & 0xFF);
+
+    digitalWrite(DAC2_CSN, HIGH);
+
+    SPI.endTransaction();
+}
+
+void dac_update_dacD(void) {
+    SPI.beginTransaction(ADC_SPISettings);
+
+    digitalWrite(DAC2_CSN, LOW);
+
+    SPI.transfer((dacD_ena ? 0xF0 : 0xE0) | (dacD_val >> 8));
+    SPI.transfer(dacD_val & 0xFF);
+
+    digitalWrite(DAC2_CSN, HIGH);
 
     SPI.endTransaction();
 }
@@ -379,7 +410,47 @@ uint8_t dac_dacB_get_ena(void) {
     return dacB_ena;
 }
 
-void dac_diff_set_val(int16_t value) {
+void dac_dacC_set_val(uint16_t value) {
+    dacC_val = value & 0x0FFF;
+    dac_update_dacC();
+    dac_load();
+}
+
+uint16_t dac_dacC_get_val(void) {
+    return dacC_val;
+}
+
+void dac_dacC_set_ena(uint8_t ena) {
+    dacC_ena = ena ? 1 : 0;
+    dac_update_dacC();
+    dac_load();
+}
+
+uint8_t dac_dacC_get_ena(void) {
+    return dacC_ena;
+}
+
+void dac_dacD_set_val(uint16_t value) {
+    dacD_val = value & 0x0FFF;
+    dac_update_dacD();
+    dac_load();
+}
+
+uint16_t dac_dacD_get_val(void) {
+    return dacD_val;
+}
+
+void dac_dacD_set_ena(uint8_t ena) {
+    dacD_ena = ena ? 1 : 0;
+    dac_update_dacD();
+    dac_load();
+}
+
+uint8_t dac_dacD_get_ena(void) {
+    return dacD_ena;
+}
+
+void dac_diffAB_set_val(int16_t value) {
     dacA_val = (uint16_t)(((0x1000 + value) >> 1) & 0xFFF);
     dacB_val = (uint16_t)(((0x1000 - value) >> 1) & 0xFFF);
     dac_update_dacA();
@@ -387,11 +458,11 @@ void dac_diff_set_val(int16_t value) {
     dac_load();
 }
 
-int16_t dac_diff_get_val(void) {
+int16_t dac_diffAB_get_val(void) {
     return (int16_t)dacA_val - (int16_t)dacB_val;
 }
 
-void dac_diff_set_ena(uint8_t ena) {
+void dac_diffAB_set_ena(uint8_t ena) {
     dacA_ena = ena ? 1 : 0;
     dacB_ena = ena ? 1 : 0;
     dac_update_dacA();
@@ -399,6 +470,30 @@ void dac_diff_set_ena(uint8_t ena) {
     dac_load();
 }
 
-uint8_t dac_diff_get_ena(void) {
+uint8_t dac_diffAB_get_ena(void) {
     return dacA_ena | dacB_ena;
+}
+
+void dac_diffCD_set_val(int16_t value) {
+    dacC_val = (uint16_t)(((0x1000 + value) >> 1) & 0xFFF);
+    dacD_val = (uint16_t)(((0x1000 - value) >> 1) & 0xFFF);
+    dac_update_dacC();
+    dac_update_dacD();
+    dac_load();
+}
+
+int16_t dac_diffCD_get_val(void) {
+    return (int16_t)dacC_val - (int16_t)dacD_val;
+}
+
+void dac_diffCD_set_ena(uint8_t ena) {
+    dacC_ena = ena ? 1 : 0;
+    dacD_ena = ena ? 1 : 0;
+    dac_update_dacC();
+    dac_update_dacD();
+    dac_load();
+}
+
+uint8_t dac_diffCD_get_ena(void) {
+    return dacC_ena | dacD_ena;
 }
--- Software/calibrate.py
+++ Software/calibrate.py
@@ -3,27 +3,25 @@
 #
 #  1. The calibration script will be run inside the bradlab environment.
 #  2. The me_gizmo class has been imported via 'import me_gizmo'.
-#  3. An me_gizmo object called 'gizmo' has been created via 'gizmo = me_gizmo.me_gizmo()'
+#  3. An me_gizmo object called 'gizmo' has been created via 'gizmo = me_gizmo.me_gizmo()' 
 #     with the device plugged into a USB port.
-#  4. A variable called num_avg has been created indicating the number of readings to average for each measurment.
+#  4. A variable calle num_avg has been created indicating the number of readings to average for each measurment.
 #  5. The calibration values will be saved to the device manually (if desired) via 'gizmo.write_calib_values()'.
 #
 # Run the script via 'exec(open('calibrate.py').read())'.
 #
-print(
-    f"Running calibration, OSR = {gizmo.adc_get_osr()}, AZ_MUX = {gizmo.adc_get_azmux()}, and num_avg = {num_avg}..."
-)
+print(f'Running calibration, OSR = {gizmo.adc_get_osr()}, AZ_MUX = {gizmo.adc_get_azmux()}, and num_avg = {num_avg}...')
 
-print("Calibrating ADC gain = 0 (Gain is x1/3)...")
+print('Calibrating ADC gain = 0 (Gain is x1/3)...')
 gizmo.adc_set_gain(0)
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_VREF])
 vrefp = gizmo.adc_get_data_avg(num_avg)
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_NEG_VREF])
 vrefm = gizmo.adc_get_data_avg(num_avg)
-gizmo.adc_slopes[0] = 5.0 / (vrefp - vrefm)
+gizmo.adc_slopes[0] = 5. / (vrefp - vrefm)
 gizmo.adc_offsets[0] = -0.5 * (vrefp + vrefm)
 
-print("Calibrating DACA...")
+print('Calibrating DACA...')
 gizmo.dacA_set_ena(1)
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_CH1_SE])
 x = linspace(0, 4000, 101)
@@ -33,23 +31,23 @@ for i in range(101):
     y[i] = gizmo.adc_get_voltage_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DACA code")
-        ylabel("ADC voltage (V)")
-        ylabel("", yaxis="right")
+        xlabel('DACA code')
+        ylabel('ADC voltage (V)')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, x, 1)
 gizmo.dacA_slope = p[0]
 gizmo.dacA_offset = p[1]
 
-plot(y, [x, polyval(p, y)], ["b.", "k-"])
-xlabel("Voltage (V)")
-ylabel("DACA code")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1]:.5e}", yaxis="right")
+plot(y, [x, polyval(p, y)], ['b.', 'k-'])
+xlabel('Voltage (V)')
+ylabel('DACA code')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating DACB...")
+print('Calibrating DACB...')
 gizmo.dacB_set_ena(1)
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_CH3_SE])
 x = linspace(0, 4000, 101)
@@ -59,23 +57,23 @@ for i in range(101):
     y[i] = gizmo.adc_get_voltage_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DACB code")
-        ylabel("ADC voltage (V)")
-        ylabel("", yaxis="right")
+        xlabel('DACB code')
+        ylabel('ADC voltage (V)')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, x, 1)
 gizmo.dacB_slope = p[0]
 gizmo.dacB_offset = p[1]
 
-plot(y, [x, polyval(p, y)], ["b.", "k-"])
-xlabel("Voltage (V)")
-ylabel("DACB code")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1]:.5e}", yaxis="right")
+plot(y, [x, polyval(p, y)], ['b.', 'k-'])
+xlabel('Voltage (V)')
+ylabel('DACB code')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating DACC...")
+print('Calibrating DACC...')
 gizmo.dacC_set_ena(1)
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_CH5_SE])
 x = linspace(0, 4000, 101)
@@ -85,23 +83,23 @@ for i in range(101):
     y[i] = gizmo.adc_get_voltage_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DACC code")
-        ylabel("ADC voltage (V)")
-        ylabel("", yaxis="right")
+        xlabel('DACC code')
+        ylabel('ADC voltage (V)')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, x, 1)
 gizmo.dacC_slope = p[0]
 gizmo.dacC_offset = p[1]
 
-plot(y, [x, polyval(p, y)], ["b.", "k-"])
-xlabel("Voltage (V)")
-ylabel("DACC code")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1]:.5e}", yaxis="right")
+plot(y, [x, polyval(p, y)], ['b.', 'k-'])
+xlabel('Voltage (V)')
+ylabel('DACC code')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating DACD...")
+print('Calibrating DACD...')
 gizmo.dacD_set_ena(1)
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_CH7_SE])
 x = linspace(0, 4000, 101)
@@ -111,23 +109,23 @@ for i in range(101):
     y[i] = gizmo.adc_get_voltage_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DACD code")
-        ylabel("ADC voltage (V)")
-        ylabel("", yaxis="right")
+        xlabel('DACD code')
+        ylabel('ADC voltage (V)')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, x, 1)
 gizmo.dacD_slope = p[0]
 gizmo.dacD_offset = p[1]
 
-plot(y, [x, polyval(p, y)], ["b.", "k-"])
-xlabel("Voltage (V)")
-ylabel("DACD code")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1]:.5e}", yaxis="right")
+plot(y, [x, polyval(p, y)], ['b.', 'k-'])
+xlabel('Voltage (V)')
+ylabel('DACD code')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating DAC DIFFAB (i.e., DACA - DACB)...")
+print('Calibrating DAC DIFFAB (i.e., DACA - DACB)...')
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_DIFFAB])
 x = linspace(-4000, 4000, 201)
 y = zeros(201)
@@ -136,23 +134,23 @@ for i in range(201):
     y[i] = gizmo.adc_get_voltage_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC voltage (V)")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC voltage (V)')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, x, 1)
 gizmo.diffAB_slope = p[0]
 gizmo.diffAB_offset = p[1]
 
-plot(y, [x, polyval(p, y)], ["b.", "k-"])
-xlabel("Voltage (V)")
-ylabel("DIFFAB code")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1]:.5e}", yaxis="right")
+plot(y, [x, polyval(p, y)], ['b.', 'k-'])
+xlabel('Voltage (V)')
+ylabel('DIFFAB code')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating DAC DIFFCD (i.e., DACC - DACD)...")
+print('Calibrating DAC DIFFCD (i.e., DACC - DACD)...')
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_DIFFCD])
 x = linspace(-4000, 4000, 201)
 y = zeros(201)
@@ -161,23 +159,23 @@ for i in range(201):
     y[i] = gizmo.adc_get_voltage_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFCD code")
-        ylabel("ADC voltage (V)")
-        ylabel("", yaxis="right")
+        xlabel('DIFFCD code')
+        ylabel('ADC voltage (V)')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, x, 1)
 gizmo.diffCD_slope = p[0]
 gizmo.diffCD_offset = p[1]
 
-plot(y, [x, polyval(p, y)], ["b.", "k-"])
-xlabel("Voltage (V)")
-ylabel("DIFFCD code")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1]:.5e}", yaxis="right")
+plot(y, [x, polyval(p, y)], ['b.', 'k-'])
+xlabel('Voltage (V)')
+ylabel('DIFFCD code')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 1 (Gain is x1)...")
+print('Calibrating ADC gain = 1 (Gain is x1)...')
 gizmo.adc_write_reg(gizmo.MUX, [gizmo.MUX_DIFFAB])
 gizmo.adc_set_gain(1)
 x = linspace(-2000, 2000, 201)
@@ -187,23 +185,23 @@ for i in range(201):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[1] = p[0]
 gizmo.adc_offsets[1] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 2 (Gain is x2)...")
+print('Calibrating ADC gain = 2 (Gain is x2)...')
 gizmo.adc_set_gain(2)
 x = linspace(-1000, 1000, 201)
 y = zeros(201)
@@ -212,23 +210,23 @@ for i in range(201):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[2] = p[0]
 gizmo.adc_offsets[2] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 3 (Gain is x4)...")
+print('Calibrating ADC gain = 3 (Gain is x4)...')
 gizmo.adc_set_gain(3)
 x = linspace(-500, 500, 201)
 y = zeros(201)
@@ -237,23 +235,23 @@ for i in range(201):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[3] = p[0]
 gizmo.adc_offsets[3] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 4 (Gain is x8)...")
+print('Calibrating ADC gain = 4 (Gain is x8)...')
 gizmo.adc_set_gain(4)
 x = linspace(-400, 400, 201)
 y = zeros(201)
@@ -262,23 +260,23 @@ for i in range(201):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[4] = p[0]
 gizmo.adc_offsets[4] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 5 (Gain is x16)...")
+print('Calibrating ADC gain = 5 (Gain is x16)...')
 gizmo.adc_set_gain(5)
 x = linspace(-160, 160, 161)
 y = zeros(161)
@@ -287,23 +285,23 @@ for i in range(161):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[5] = p[0]
 gizmo.adc_offsets[5] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 6 (Gain is x32)...")
+print('Calibrating ADC gain = 6 (Gain is x32)...')
 gizmo.adc_set_gain(6)
 x = linspace(-80, 80, 161)
 y = zeros(161)
@@ -312,23 +310,23 @@ for i in range(161):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[6] = p[0]
 gizmo.adc_offsets[6] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
 
-print("Calibrating ADC gain = 7 (Gain is x64)...")
+print('Calibrating ADC gain = 7 (Gain is x64)...')
 gizmo.adc_set_gain(7)
 x = linspace(-40, 40, 81)
 y = zeros(81)
@@ -337,18 +335,19 @@ for i in range(81):
     y[i] = gizmo.adc_get_data_avg(num_avg)
     if i > 0:
         plot(x[:i], y[:i])
-        xlabel("DIFFAB code")
-        ylabel("ADC code")
-        ylabel("", yaxis="right")
+        xlabel('DIFFAB code')
+        ylabel('ADC code')
+        ylabel('', yaxis = 'right')
         draw_now()
 
 p = polyfit(y, (x - gizmo.diffAB_offset) / gizmo.diffAB_slope, 1)
 gizmo.adc_slopes[7] = p[0]
 gizmo.adc_offsets[7] = p[1] / p[0]
 
-plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ["b.", "k-"])
-xlabel("ADC code")
-ylabel("DIFFAB voltage (V)")
-ylabel(f"slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}", yaxis="right")
+plot(y, [(x - gizmo.diffAB_offset) / gizmo.diffAB_slope, polyval(p, y)], ['b.', 'k-'])
+xlabel('ADC code')
+ylabel('DIFFAB voltage (V)')
+ylabel(f'slope = {p[0]:.5e}, offset = {p[1] / p[0]:.5e}', yaxis = 'right')
 draw_now()
-input("Press enter to continue.")
+input('Press enter to continue.')
+
